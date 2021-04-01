49621c90cd53471cc38074c1fcad86719b851ad2
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2021-04-01 14:29:55 +0300

    Add files via upload

---
+++ 1180505018/1180505018/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace _1180505018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Soru1 = new System.Windows.Forms.Button();
+            this.Soru2 = new System.Windows.Forms.Button();
+            this.Soru3 = new System.Windows.Forms.Button();
+            this.Cevap = new System.Windows.Forms.Label();
+            this.txtCevap = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Soru1
+            // 
+            this.Soru1.Location = new System.Drawing.Point(44, 69);
+            this.Soru1.Name = "Soru1";
+            this.Soru1.Size = new System.Drawing.Size(75, 29);
+            this.Soru1.TabIndex = 0;
+            this.Soru1.Text = "Soru-1";
+            this.Soru1.UseVisualStyleBackColor = true;
+            this.Soru1.Click += new System.EventHandler(this.Soru1_Click);
+            // 
+            // Soru2
+            // 
+            this.Soru2.Location = new System.Drawing.Point(44, 104);
+            this.Soru2.Name = "Soru2";
+            this.Soru2.Size = new System.Drawing.Size(75, 29);
+            this.Soru2.TabIndex = 1;
+            this.Soru2.Text = "Soru-2";
+            this.Soru2.UseVisualStyleBackColor = true;
+            this.Soru2.Click += new System.EventHandler(this.Soru2_Click);
+            // 
+            // Soru3
+            // 
+            this.Soru3.Location = new System.Drawing.Point(44, 139);
+            this.Soru3.Name = "Soru3";
+            this.Soru3.Size = new System.Drawing.Size(75, 29);
+            this.Soru3.TabIndex = 2;
+            this.Soru3.Text = "Soru-3";
+            this.Soru3.UseVisualStyleBackColor = true;
+            this.Soru3.Click += new System.EventHandler(this.Soru3_Click);
+            // 
+            // Cevap
+            // 
+            this.Cevap.AutoSize = true;
+            this.Cevap.Location = new System.Drawing.Point(226, 77);
+            this.Cevap.Name = "Cevap";
+            this.Cevap.Size = new System.Drawing.Size(38, 13);
+            this.Cevap.TabIndex = 3;
+            this.Cevap.Text = "Cevap";
+            // 
+            // txtCevap
+            // 
+            this.txtCevap.Location = new System.Drawing.Point(195, 113);
+            this.txtCevap.Name = "txtCevap";
+            this.txtCevap.Size = new System.Drawing.Size(100, 20);
+            this.txtCevap.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 280);
+            this.Controls.Add(this.txtCevap);
+            this.Controls.Add(this.Cevap);
+            this.Controls.Add(this.Soru3);
+            this.Controls.Add(this.Soru2);
+            this.Controls.Add(this.Soru1);
+            this.Name = "Form1";
+            this.Text = "1180505018";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Soru1;
+        private System.Windows.Forms.Button Soru2;
+        private System.Windows.Forms.Button Soru3;
+        private System.Windows.Forms.Label Cevap;
+        private System.Windows.Forms.TextBox txtCevap;
+    }
+}
+
---
+++ 1180505018/1180505018/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1180505018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Soru1_Click(object sender, EventArgs e)
+        {
+            long sayi = 123456;
+            bool asalMi = true;
+
+            for(long i = sayi; i >= 1; i--)
+            {
+                if(sayi % i == 0)
+                {
+                    for(long j = 2; j < i; j++)
+                    {
+                        if(i % j == 0)
+                        {
+                            asalMi = false;
+                            break;
+                        }
+                    }
+
+                    if (asalMi)
+                    {
+                        txtCevap.Text = i.ToString();
+                        break;
+                    }
+                    asalMi = true;
+                }
+            }
+
+        }
+
+        private void Soru2_Click(object sender, EventArgs e)
+        {
+
+            // öklit algoritmasını oluşturuyoruz
+            int GCD(int a, int b)
+            {
+                if(b == 0){ return a; }
+                else{ return GCD(b, a % b); }
+            }
+
+            // öklit algoritamasını kullanarak en küçük ortak katı buluyoruz
+            int LCM(int a, int b)
+            {
+                var gcd = GCD(a, b);
+                return a * b / gcd;
+            }
+
+            int result = 1;
+            string res;
+
+            // 1 her sayıya bölündüğü için 2-20 arası en küçük ortak katı buluyoruz
+            for(int n = 2; n < 20; n++)
+            {
+                result = LCM(result, n);
+            }
+
+            res = result.ToString();
+            txtCevap.Text = res;
+            
+
+        }
+
+        private void Soru3_Click(object sender, EventArgs e)
+        {
+            int length = 0;
+            string  SayiToYazi(string sayi)
+            {
+                int max_basamak = 30;
+                int a;
+                string[] answer;
+                string[] birler = { "", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" };
+                string[] onlar = { "", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" };
+                string[] binler = { "oktilyon", "septilyon", "seksilyon", "kentilyon", "katrilyon", "trilyon", "milyar", "milyon", "bin", "" };
+                int[] basamaklar = new int[3];
+                string sonuc = "";
+                //sayının kullanılmayan basamaklarını sıfırla doldur
+                sayi = sayi.PadLeft(max_basamak, '0');
+                //sayıyı üçerli basamaklara ayır
+                for (int i = 0; i < max_basamak / 3; i++)
+                {
+                    string temp = "";
+                    //yüzler basamağı
+                    basamaklar[0] = (int)(sayi[i * 3] - '0');
+                    //onlar basamağı
+                    basamaklar[1] = (int)(sayi[i * 3 + 1] - '0');
+                    //birler basamağı
+                    basamaklar[2] = (int)(sayi[i * 3 + 2] - '0');
+
+                    if (basamaklar[0] == 0)
+                        temp = ""; //yüzler basamağı boş
+                    else
+                        if (basamaklar[0] == 1)
+                        temp = "yüz"; //yüzler basamağında 1 varsa
+                    else
+                        temp = birler[basamaklar[0]] + "yüz";  // birleştir
+
+                    //yüzler+onlar+birler basamağını birleştir
+                    temp += onlar[basamaklar[1]] + birler[basamaklar[2]];
+
+                    //basamak değeri oluşmadıysa yani 000 ise binler basamağını ekle
+                    if (!string.IsNullOrEmpty(temp)) temp += binler[i];
+                    //birbin olmaz
+                    if ((i > 1) && (temp.Equals("birbin"))) temp = "bin";
+                    if (temp != "") sonuc += temp + "";
+                }
+                if (string.IsNullOrEmpty(sonuc.Trim()))
+                    sonuc = "sıfır";
+                return sonuc.Trim();
+            }
+
+            for (int a = 0; a <= 1000; a++)
+            {
+                //Console.WriteLine(SayiToYazi(a.ToString()));
+                length += SayiToYazi(a.ToString()).Length;
+            }
+
+            txtCevap.Text = length.ToString();
+        }
+    }
+}
